4641403bd4d8903bd113ba8850f3b7edef8990ce
kadiamoon <117816839+kadiamoon@users.noreply.github.com>
2022-11-10 11:22:44 +0600

    Update main.js

--- main.js
+++ main.js
@@ -6,37 +6,19 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'vertical',
-//     loop: true,
-  
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
-var swiper = new Swiper(".home- s1ider", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-            delay: 7500,
-            disableonInteraction: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        loop: true,
-    })
+// var swiper = new Swiper(".home- s1ider", {
+//         spaceBetween: 30,
+//         centeredSlides: true,
+//         autoplay: {
+//             delay: 7500,
+//             disableonInteraction: false,
+//         },
+//         pagination: {
+//             el: ".swiper-pagination",
+//             clickable: true,
+//         },
+//         loop: true,
+//     })
+
+// // const activePage = window.location.pathname;
+// console.log(activePage);
